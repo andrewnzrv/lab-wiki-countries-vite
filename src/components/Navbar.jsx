d1c722fb709e61a80286cd4da84848c7aa4c4474
Andrei Nazarov <andrei.nazarov@tum.de>
2024-01-02 20:28:14 +0100

    Adde Home Page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,13 @@
-function Navbar() {}
+function Navbar() {
+  return (
+    <nav class="navbar navbar-dark bg-primary mb-3">
+      <div class="container">
+        <a class="navbar-brand" href="/">
+          WikiCountries
+        </a>
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar;
